9acd0934fea7cc3f988882b0bb6a86bc816c20a4
Matthew Einhorn <moiein2000@gmail.com>
2020-10-08 18:49:28 -0400

    Fix parsing older files.

--- more_kivy_app/utils.py
+++ more_kivy_app/utils.py
@@ -3,8 +3,8 @@
 """
 from kivy.properties import ObservableDict, ObservableList, Property
 from functools import partial
-from tree_config.utils import yaml_loads, get_yaml as orig_get_yaml, \
-    yaml_dumps as orig_yaml_dumps
+from tree_config.utils import yaml_loads as orig_yaml_loads,\
+    get_yaml as orig_get_yaml, yaml_dumps as orig_yaml_dumps
 
 __all__ = ('get_yaml', 'yaml_dumps', 'yaml_loads')
 
@@ -24,6 +24,16 @@ def get_yaml():
 
     yaml.representer.add_multi_representer(Property, represent_property)
     return yaml
+
+
+def yaml_loads(value, get_yaml_obj=get_yaml):
+    # somehow in older versions, b'yuv420p' got encoded to
+    # '!!binary |\neXV2NDIwcA==\n' instead of '!!binary |\n eXV2NDIwcA==\n'.
+    # So now it can't be parsed. Hence add the space
+    if len(value) >= 12 and value.startswith('!!binary |\n') and \
+            value[11] != ' ':
+        value = value[:11] + ' ' + value[11:]
+    return orig_yaml_loads(value, get_yaml_obj=get_yaml_obj)
 
 
 yaml_dumps = partial(orig_yaml_dumps, get_yaml_obj=get_yaml)
